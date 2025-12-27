0b5ccdbbb7a7fa27b6fbb612cb76de6017f3a628
User <user@example.com>
2025-12-28 01:09:38 +0900

    fix: 更にサブセット修正

--- app/lib/cardFonts.ts
+++ app/lib/cardFonts.ts
@@ -3,19 +3,19 @@ import type { CardFontStyle } from "./schema";
 
 const cardFontNormal = Noto_Sans_JP({
   weight: ["400", "600", "700"],
-  subsets: ["japanese"],
+  subsets: ["latin"],
   display: "swap",
 });
 
 const cardFontBold = Mochiy_Pop_One({
   weight: "400",
-  subsets: ["japanese"],
+  subsets: ["latin"],
   display: "swap",
 });
 
 const cardFontHandwritten = Yomogi({
   weight: "400",
-  subsets: ["japanese"],
+  subsets: ["latin"],
   display: "swap",
 });
 
